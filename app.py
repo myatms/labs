0bd475f2011a8f2262abea301e7e0cba8211e90c
myatms <ethan.mms01@gmail.com>
2022-09-03 12:08:16 +0630

    travis_setup

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/',methods=['GET'])
+def home():
+     return "<h1>Welcome to my channel</h1>"
+if __name__=='__main__':
+     app.run(debug=True,host="0.0.0.0")
